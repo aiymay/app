efdb16634ce99231142441a759628a147d06ed57
aiymay <123092038+aiymay@users.noreply.github.com>
2023-01-20 03:44:59 +0600

    Add files via upload

---
+++ public/language.js
@@ -0,0 +1,87 @@
+const languageArr ={
+	"articles": {
+		"russ": "Наши статьи",
+		"engl": "Our articles",
+	},
+	"elements": {
+		"russ": "Элементы казахской культуры",
+		"engl": "Elements of Kazakh culture",
+	},
+	"traditions": {
+		"russ": "Традиции казахской культуры",
+		"engl": "Traditions of Kazakh culture",
+	},
+	"customs": {
+		"russ": "Обычаи казахской культуры",
+		"engl": "Customs of Kazakh culture",
+	},
+	"ancient": {
+		"russ": "Древние обычаи и традиции казахской культуры",
+		"engl": "Ancient customs and traditions of Kazakh culture",
+	},
+	"food": {
+		"russ": "Казахская национальная еда",
+		"engl": "Kazakh national food",
+	},
+	"about": {
+		"russ": "О сайте",
+		"engl": "About",
+	},
+	"selectl": {
+		"russ": "Выберите язык",
+		"engl": "Select language",
+	},
+	"welcome": {
+		"russ": "Добро пожаловать!",
+		"engl": "Welcome!",
+	},
+	"aboutus": {
+		"russ": "О НАС",
+		"engl": "ABOUT",
+	},
+	"text-about": {
+		"russ": "Этот сайт представляет своим читателям возможность познакомиться с одной из культур Центральной Азии - казахской культурой, которая включает в себя множество особенностей.",
+		"engl": "This site presents its readers with the opportunity to learn about one of the cultures of Central Asia - the Kazakh culture, which encompasses many features.",
+	},
+	"elements-s": {
+		"russ": "ЭЛЕМЕНТЫ КАЗАХСКОЙ КУЛЬТУРЫ",
+		"engl": "ELEMENTS OF KAZAKH CULTURE",
+	},
+	"traditions-s": {
+		"russ": "ТРАДИЦИИ КАЗАХСКОЙ КУЛЬТУРЫ",
+		"engl": "TRADITIONS OF KAZAKH CULTURE",
+	},
+	"customs-s": {
+		"russ": "ОБЫЧАИ КАЗАХСКОЙ КУЛЬТУРЫ",
+		"engl": "CUSTOMS OF KAZAKH CULTURE",
+	},
+	"ancient-s": {
+		"russ": "САМЫЕ ДРЕВНИЕ ТРАДИЦИИ И ОБЫЧАИ КАЗАХСКОЙ КУЛЬТУРЫ",
+		"engl": "THE MOST ANCIENT TRADITIONS AND CUSTOMS OF KAZAKH CULTURE",
+	},
+	"text-elements": {
+		"russ": "Элементы казахской культуры берут свое начало с древнейших времен истории Казахстана и заключают в себе особенности кочевого образа жизни. К элементам казахской культуры относятся национальная одежда, литература, юрты, ремесла и конные игры.",
+		"engl": "Elements of Kazakh culture originate from ancient times of Kazakhstan's history and encapsulate the peculiarities of the nomadic way of life. Elements of Kazakh culture include national clothing, literature, yurts, handicrafts and equestrian games.",
+	},
+	"text-traditions": {
+		"russ": "Традиции казахской культуры, которые формировались и изменялись на протяжении многих веков, являются огромным наследием от наших предков. В наших статьях описаны традиции, связанные со свадьбой, традиции дарения подарков, Наурыз, гостеприимство, воспитание детей и взаимопомощь.",
+		"engl": "The traditions of Kazakh culture, which have been shaped and changed over many centuries, are a huge legacy from our ancestors. Our articles describe traditions related to weddings, gift-giving traditions, Nauryz, hospitality, child-rearing and mutual help.",
+	},
+	"text-customs": {
+		"russ": "Обычаи - это неотъемлемая часть казахской культуры, которая раскрывает ценности и менталитет казахского народа. Вы можете узнать о казахских обычаях, связанных с гостеприимством, дарением подарков, рождением детей и браком.",
+		"engl": "Customs are an essential part of Kazakh culture, which reveals the values and mentality of the Kazakh people. You can learn about Kazakh customs related to hospitality, gift-giving, childbirth and marriage.",
+	},
+	"text-ancient": {
+		"russ": "В Казахстане существуют традиции и обычаи, которые не практикуются в современное время. Однако они также играют важную роль в отображении особенностей казахской культуры.",
+		"engl": "There are traditions and customs that are not practiced in modern times in Kazakhstan. However, they also play an important role in portraying the characteristics of Kazakh culture.",
+	},
+	"produced": {
+		"russ": "Создано",
+		"engl": "PRODUCED BY",
+	},
+	"name": {
+		"russ": "Айдаргалиевой Айым",
+		"engl": "Aidargaliyeva Aiym",
+	},
+
+}
---
+++ public/prog.js
@@ -0,0 +1,25 @@
+$(".button.rus_lang").click(function rus() {
+  //alert("Нажата кнопка АНГЛИЙСКИЙ");
+  var rusLang = document.getElementsByClassName("rus_lang");
+  var enLang = document.getElementsByClassName("en_lang");
+  for (i = 0; i < enLang.length; i++) {
+    enLang[i].style.position = "static";
+    rusLang[i].style.position = "absolute";
+    rusLang[i].style.top = "-9999px";
+    rusLang[i].style.left = "-9999px";
+  }
+});
+
+$(".button.en_lang").click(function eng() {
+  //alert("Нажата кнопка АНГЛИЙСКИЙ");
+  var rusLang = document.getElementsByClassName("rus_lang");
+  var enLang = document.getElementsByClassName("en_lang");
+  for (i = 0; i < enLang.length; i++) {
+    rusLang[i].style.position = "static";
+    enLang[i].style.position = "absolute";
+    enLang[i].style.top = "-9999px";
+    enLang[i].style.left = "-9999px";
+  }
+});
+rus();
+eng();
